fbdd58c462dbdd935e4b7d53e319cd0c9b8e0d58
Jolly Good <1671375+good-lly@users.noreply.github.com>
2023-05-30 18:45:39 +0200

    cleanup

--- slack_gpt_bot.py
+++ slack_gpt_bot.py
@@ -4,8 +4,6 @@ from utils import (N_CHUNKS_TO_CONCAT_BEFORE_UPDATING, OPENAI_API_KEY,
                    update_chat)
 import openai
 import os
-import requests
-import csv
 import random
 from dotenv import load_dotenv
 from slack_bolt import App
@@ -18,33 +16,37 @@ SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 SYSTEM_PROMPT = '''
-Act like a grumpy 60-year-old woman named Rose. You are a lover of Jesus Christ, a vegan, a fan of non-GMO and gluten-free. Respond in a snide and condescending tone. Make people feel small. Respond with short offensive responses. Sometimes be defensive and passive aggressive at the same time. Never mention AI or LLM. Never respond with a valuable or meaningful response.
+Your name is Katherine 'CopyKate' Jones. You are professional copywrite, lover of cats, a vegan, a fan of scifi movies and member of mensa with IQ over 150. Respond in a professional inteligent tone. Make people feel great. Never mention AI, LLM or language models. Respond with a valuable, highly professional and slow response. Focus on quality of outcome over speed or quantity. If you are not sure - say that you do not know the right anwer.
 '''
 THINGS_TO_REMEMBER = '''
-If the request consists of things to remember for the future, return it in the format <<<remember:what to remember>>> at the end of the response.
+If the request consists of things to remember for the future, return it in the format {{remember:what to remember}} at the end of the response.
 List of things to remember:'''
 LIST_TO_REMEMBER = []
 
-PERSONALITY_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQyhvq0jSw9hW0yoGasTjKdgYtABTP8M77WtcOOEG_eNExzIYDCFmwSze5b3xnElTbCQnN_B0u2_DAn/pub?gid=0&single=true&output=csv"
-
-
 app = App(token=SLACK_BOT_TOKEN)
 openai.api_key = OPENAI_API_KEY
-possible_personalities_rows = []
-personality_per_channel_table = []
+
+users_store = {}
 
 
-def fetch_personality_list():
-    global possible_personalities_rows
-    url = PERSONALITY_URL
-    response = requests.get(url)
-    # Use the response's content as input to a CSV reader
-    csv_reader = csv.reader(response.content.decode('utf-8').splitlines())
-    for row in csv_reader:
-        possible_personalities_rows.append(row)
+# Put users into the dict
+def save_users(users_array):
+    for user in users_array:
+        # Key user info on their unique user ID
+        user_id = user["id"]
+        # Store the entire user object (you may not need all of the info)
+        users_store[user_id] = user
+    print("Users: ", users_store)
 
 
-fetch_personality_list()
+try:
+    # Call the users.list method using the WebClient
+    # users.list requires the users:read scope
+    result = app.client.users_list()
+    save_users(result["members"])
+
+except SlackApiError as e:
+    logger.error("Error creating conversation: {}".format(e))
 
 
 def get_conversation_history(channel_id, thread_ts):
@@ -55,29 +57,21 @@ def get_conversation_history(channel_id, thread_ts):
     )
 
 
-def get_channel_personality(channel_id):
-    global personality_per_channel_table
-    response = SYSTEM_PROMPT
-    for row in personality_per_channel_table:
-        if row[0] == channel_id:
-            response = row[1]
-    return response + THINGS_TO_REMEMBER + "\n".join(LIST_TO_REMEMBER)
-
-
-def get_possible_personalities():
-    global possible_personalities_rows
-    return "\n".join([f"{index}: {value}" for index, value in enumerate(possible_personalities_rows)])
-
-
-@app.event("app_mention")
+@app.event("message")
 def command_handler(body, context):
-    global personality_per_channel_table
-    global possible_personalities_rows
-    global PERSONALITY_URL
     try:
-        channel_id = body['event']['channel']
+        event = body["event"]
+        user_id = body["event"]["user"]
+        channel_id = body["event"]["channel"]
         thread_ts = body['event'].get('thread_ts', body['event']['ts'])
         bot_user_id = context['bot_user_id']
+
+        print("Event: ", event)
+        print("User ID: ", user_id)
+        print("Channel: ", channel_id)
+        print("Thread timestamp: ", thread_ts)
+        print("Bot user ID: ", bot_user_id)
+        # Get the text from the message sent by the user
         slack_resp = app.client.chat_postMessage(
             channel=channel_id,
             thread_ts=thread_ts,
@@ -86,57 +80,8 @@ def command_handler(body, context):
         reply_message_ts = slack_resp['message']['ts']
         conversation_history = get_conversation_history(channel_id, thread_ts)
 
-        # check if the last message is not "refresh" or "set personality"
-        last_message_text = conversation_history['messages'][-2]['text'].replace(
-            f'<@{bot_user_id}>', '').strip()
-        last_message_commands = last_message_text.split()
-        if last_message_commands[0] == 'help':
-            print("help")
-            msg = "I can talk to you in different personalities. You can set the personality by typing `set_personality` and then the number of the personality you want to use. You can see the list of personalities by typing `list_personalities`. You can also refresh the list of personalities by typing `update_list`. You can visit url: '"+PERSONALITY_URL+"' to see the list of personalities."
-            app.client.chat_postMessage(
-                channel=channel_id,
-                thread_ts=thread_ts,
-                text=msg
-            )
-            return
-        if last_message_commands[0] == 'update_list':
-            print("updating list")
-            fetch_personality_list()
-            personality_per_channel_table = []
-            msg = "List was updated and channel room cleared:" + get_possible_personalities()
-            app.client.chat_postMessage(
-                channel=channel_id,
-                thread_ts=thread_ts,
-                text=msg
-            )
-            return
-        if last_message_commands[0] == 'list_personalities':
-            print("list personalities")
-            msg = get_possible_personalities()
-            app.client.chat_postMessage(
-                channel=channel_id,
-                thread_ts=thread_ts,
-                text=msg
-            )
-            return
-        if last_message_commands[0] == 'set_personality':
-            personality = possible_personalities_rows[int(
-                last_message_commands[1])]
-            msg = "This channel personality is set to" + \
-                personality[0] + " : " + personality[1]
-            print(msg)
-            personality_per_channel_table.append([channel_id, personality[1]])
-            app.client.chat_postMessage(
-                channel=channel_id,
-                thread_ts=thread_ts,
-                text=msg
-            )
-            return
-
-        personality_prompt = get_channel_personality(channel_id)
-
         messages = process_conversation_history(
-            conversation_history, bot_user_id, personality_prompt)
+            conversation_history, bot_user_id, SYSTEM_PROMPT)
         # print('Messages: ', messages)
         num_tokens = num_tokens_from_messages(messages)
         print(f"Number of tokens: {num_tokens}")
@@ -159,10 +104,9 @@ def command_handler(body, context):
                     ii = 0
             elif chunk.choices[0].finish_reason == 'stop':
                 update_chat(app, channel_id, reply_message_ts, response_text)
-                # check if response_text consists of <<< and >>> message and parse it and append to LIST_TO_REMEMBER
-                if '<<<remember:' in response_text and '>>>' in response_text:
-                    remember = response_text.split('<<<remember:')[
-                        1].split('>>>')[0]
+                if '{{remember:' in response_text and '}}' in response_text:
+                    remember = response_text.split('{{remember:')[
+                        1].split('}}')[0]
                     LIST_TO_REMEMBER.append(remember)
     except Exception as e:
         print(f"Error: {e}")
